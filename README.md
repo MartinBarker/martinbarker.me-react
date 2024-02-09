0779f874020601143e5c9cd848d8b3103c451cd5
Martin Barker <martinbarker99@gmail.com>
2024-02-09 14:08:55 -0800

    firebase update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Firebase auth project name: Colum Code Sprint B
-# Fill out .env 
+# How to setup with a new firebase project:
+- Go to firebase website, create new project
+- Add web app
+- Develop -> Cloud Firestore -> Create database (production mode)
+- Enable google adsense tracking for 'measurementId' var
+- Project settings, scroll down, your apps -> web apps
+- Copy vars from npm sdk setup and add to new file `.env` (view `.env_template` for formatting reference)
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,6 @@
 import firebase from "firebase/compat/app"
 import "firebase/compat/auth"
 
-
-
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -10,7 +8,8 @@ const app = firebase.initializeApp({
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  })
+  measurementId: process.env.MEASUREMENT_ID
+})
 
-  export const auth = app.auth()
-  export default app
+export const auth = app.auth()
+export default app
